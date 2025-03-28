df8643ca05978c58c433e4af17e52db2b061735f
Namrata Jaiswal <namratajaiswal047@gmail.com>
2025-03-29 03:06:13 +0530

    Test commit

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,32 +1,76 @@
-import React from "react";
-import "./Card.css";
+import React from 'react'
+import styles from './Card.module.css';
+import {Tooltip , Chip} from '@mui/material'
+const Card = ({data,type}) => {
+    
+  const getCard = (type) =>{
+    switch(type){
+        case "album":{
+            const {image,follows,title,songs} = data;
+            return (
+                <Tooltip title={`${songs.length} songs`} placement='top' arrow >
+                    <div className={styles.wrapper}>
+                        <div className={styles.card}>
+                            <img src={image} alt='album'/>
+                            <div className={styles.banner}>
+                                <Chip label={`${follows} Follows`} size='small' className={styles.chip} />
+                            </div>
+                        </div>
+                        <div className={styles.titleWrapper}>
+                            <p>{title}</p>
+                        </div>
+                    </div>
+                </Tooltip>
+            )
+        }
 
-/**
- * Represents a card component.
- * Renders a card based on the provided data and type.
- * @param {Object} data - The data object containing information for the card.
- * @param {string} type - The type of the card.
- * @returns {JSX.Element|null} The rendered card component or null if the type is not recognized.
- */
-const Card = ({ data, type }) => {
-  switch (type) {
-    case "normal":
-      return (
-        <div className="Album_Cont" id={data.id}>
-          <div className="Album_card">
-            <div className="Album_card_image">
-              <img src={data.image} alt={data.title} />
-            </div>
-            <div className="Album_card_text">
-              <h3>{data.follows} Follows</h3>
-            </div>
-          </div>
-          <h3 className="Album_title">{data.title}</h3>
-        </div>
-      );
-    default:
-      return null;
+        case "song" : {
+            const {image,likes,title,songs} = data;
+            return (
+              <Tooltip title={`${songs?.length} songs`} placement='top' arrow>
+              <div className={styles.wrapper}>
+                <div className={styles.card}>
+                  <img src={image} alt='song' loading='lazy' />
+                  <div className={styles.banner}>
+                  <div className={styles.banner}>
+                                <Chip label={`${likes} Likess`} size='small' className={styles.chip} />
+                            </div>
+                  </div>
+                </div>
+                <div className={styles.titleWrapper}>
+                  <p>{title}</p>
+                </div>
+              </div>
+              </Tooltip>
+            )
+          }
+
+          case 'songFilter' : {
+            const {image,likes,title} = data;
+            return (
+            <div className={styles.wrapper}>
+                <div className={styles.card}>
+                  <img src={image} alt='song' loading='lazy' />
+                  <div className={styles.banner}>
+                  <div className={styles.banner}>
+                                <Chip label={`${likes} Likes`} size='small' className={styles.chip} />
+                            </div>
+                  </div>
+                </div>
+                <div className={styles.titleWrapper}>
+                  <p>{title}</p>
+                </div>
+              </div>
+                        
+            )
+          }
+
+          
+        default:
+            return <></>;
+    }
   }
-};
+  return getCard(type);
+}
 
-export default Card;
+export default Card
